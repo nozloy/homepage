90b18f639ba4253b8bca75e2b7b9d858da344fdd
nozloy <31240637+nozloy@users.noreply.github.com>
2022-05-22 03:34:37 +0300

    import framer-motion

--- src/components/card.js
+++ src/components/card.js
@@ -2,27 +2,47 @@ import React from 'react'
 import avatar from '../assets/images/avatar.jpg'
 import Links from './links'
 import About from './about'
+import { motion } from 'framer-motion'
 
 const Card = () => {
 	return (
 		<div className='w-full scroll-smooth'>
-			<div
-				className='flex flex-col justify-center max-w-sm mx-auto
-             bg-white shadow-xl rounded-xl p-5 sm:max-w-xl'
+			<motion.div
+				initial={{ y: -500 }}
+				animate={{ y: 0 }}
+				transition={{ delay: 0.4, type: 'spring', ease: 'easeOut' }}
 			>
-				<img
-					className='w-32 drop-shadow-sm mx-auto shadow-xl rounded-full sm:w-48'
-					src={avatar}
-					alt='avatar'
-				/>
-				<div className='text-slate-500 text-center m-1 p-3 text-md border-b-2 sm:text-xl'>
-					<h1 className='text-slate-700 text-xl sm:text-2xl'>Almir Sadykoff</h1>
-					<h2>Web-developer</h2>
-					<h2>3D-artist</h2>
+				<div
+					className='flex flex-col justify-center max-w-sm mx-auto
+             bg-white shadow-xl rounded-xl p-5 sm:max-w-xl'
+				>
+					<img
+						className='w-32 drop-shadow-sm mx-auto shadow-xl rounded-full sm:w-48'
+						src={avatar}
+						alt='avatar'
+					/>
+					<div className='text-slate-500 text-center m-1 p-3 text-md border-b-2 sm:text-xl'>
+						<h1 className='text-slate-700 text-xl sm:text-2xl'>
+							Almir Sadykoff
+						</h1>
+						<h2>Web-developer</h2>
+						<h2>3D-artist</h2>
+					</div>
+					<Links />
 				</div>
-				<Links />
-			</div>
-			<About />
+			</motion.div>
+			<motion.div
+				initial={{ y: 500 }}
+				animate={{ y: 0 }}
+				transition={{
+					delay: 0.9,
+					type: 'spring',
+					ease: 'easeOut',
+					damping: 15,
+				}}
+			>
+				<About />
+			</motion.div>
 		</div>
 	)
 }
