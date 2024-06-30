42146c1f793c8672a13d47142699912370580a0d
NoZloy <nozloy@gmail.com>
2024-07-01 00:57:09 +0300

    removing three.js

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ function App() {
 	return (
 		<div>
 			<div className='snap-mandatory snap-y overflow-y-scroll h-screen no-scrollbar'>
-				{/* <div className='snap-always snap-start bg-gray-100 h-screen py-10 px-3 sm:px-5 select-none'>
+				<div className='snap-always snap-start bg-gray-100 h-screen py-10 px-3 sm:px-5 select-none'>
 					<Card />
-				</div> */}
-				<div className='snap-always snap-center bg-white h-screen py-10 px-3 sm:px-5 '>
-					<Model />
 				</div>
-				{/* <div className='snap-always snap-start bg-gray-100 h-screen py-10 px-3 sm:px-5 select-none'>
-					<Skills />
+				{/* <div className='snap-always snap-center bg-white h-screen py-10 px-3 sm:px-5 '>
+					<Model />
 				</div> */}
+				<div className='snap-always snap-start bg-gray-100 h-screen py-10 px-3 sm:px-5 select-none'>
+					<Skills />
+				</div>
 			</div>
 		</div>
 	)
